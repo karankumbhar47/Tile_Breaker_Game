50180f98f1018ccf70b496a8bd98d0afc0a066d8
Sanket-Dandge <srdandge2003@gmail.com>
2023-03-07 17:13:32 +0530

    Added tile object
    Added three different tiles
    Set the positions of the tiles

--- TileBreaker.py
+++ TileBreaker.py
@@ -5,10 +5,9 @@ import random
 # initializing pygame
 pygame.init()
 
-
-# creating screen 
-screen_hieght = 600
-screen_width = 800
+# creating screen
+screen_hieght = 800
+screen_width = 836
 
 screen = pygame.display.set_mode((screen_width,screen_hieght))
 
@@ -19,10 +18,10 @@ game_icon = pygame.image.load('./images/brick-breaker.png')
 pygame.display.set_icon(game_icon)
 
 
-# slider 
+# slider
 sliderImg = pygame.image.load('./images/53-Breakout-Tiles.png')
 sliderX = 0
-sliderY = 550
+sliderY = screen_hieght - 50
 sliderX_change = 0
 
 # function to build slider image
@@ -31,41 +30,75 @@ def slider(x,y):
 
 
 
+# Tiles
+# loading tiles images
+mudTileImg = pygame.image.load('./images/19-Breakout-Tiles.png')
+steelTileImg = pygame.image.load('./images/17-Breakout-Tiles.png')
+unbreakableTileImg = pygame.image.load('./images/07-Breakout-Tiles.png')
+
+# creating co-ordinates for tiles placing
+tileWidth = 106
+tileHeight = 28
+startTileX = [100,153]
+startTileY = 100
+tileXpointsA = [startTileX[0]+(tileWidth*i) for i in range(0,6)]
+tileXpointsB = [startTileX[1]+(tileWidth*i) for i in range(0,5)]
+tileYpoints = [startTileY+(tileHeight*i) for i in range(0,11)]
+
+# storing position of tiles in tilePosition array
+tilePositionArray = []
+for i in range(len(tileYpoints)):
+    for j in range(len(tileXpointsA)):
+        randomTile = random.choice([mudTileImg,steelTileImg,unbreakableTileImg])
+        tilePositionArray.append([tileXpointsA[j],tileYpoints[i],randomTile])
+
+#function to build tiles
+def tile(x, y, tileImg):
+    screen.blit(tileImg, (x, y))
+
+
+
+
 # to keep window alive running while loop
 running = True
 while running:
 
-    #screen color 
+    #screen color
     screen.fill((0,0,0))
 
 
-    # checking event to quit window 
+    # checking event to quit window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        #adding keys to move slider left and right.
+        # adding keys to move slider left and right
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 sliderX_change = -0.6
             if event.key == pygame.K_RIGHT:
                 sliderX_change = 0.6
-        
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 sliderX_change = 0
-                
-    
-    #slider controlling movement at the end of screen
+
+
+    # controlling movement of slider at the end of screen
     sliderX += sliderX_change
     if sliderX <=0 :
         sliderX = 0
-    elif sliderX >= 680:
-        sliderX = 680
+    elif sliderX >= screen_width - 120 :
+        sliderX = screen_width - 120
 
-    #rendering slider.
+    #building slider
     slider(sliderX,sliderY)
-            
+
+
+    #building tiles
+    for i in range(len(tilePositionArray)):
+        tile(tilePositionArray[i][0],tilePositionArray[i][1],tilePositionArray[i][2])
+
 
     #updating display every time
     pygame.display.update()
