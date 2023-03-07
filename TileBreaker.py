b1116310bcf2b1cfbd5d5f71c0a4b0e89accdd82
SandeshAhire <ahiresandesh@iitbhilai.ac.in>
2023-03-07 16:01:00 +0530

    Added slider object
    Created slider.
    Added movement to slider.

--- TileBreaker.py
+++ TileBreaker.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 
-#initializing pygame
+# initializing pygame
 pygame.init()
 
 
@@ -15,8 +15,19 @@ screen = pygame.display.set_mode((screen_width,screen_hieght))
 
 # setting title and game icon
 pygame.display.set_caption("Tile Breaker")
-game_icon = pygame.image.load('/home/karan09/coding/Python/pygame/images/brick-breaker.png')
+game_icon = pygame.image.load('./images/brick-breaker.png')
 pygame.display.set_icon(game_icon)
+
+
+# slider 
+sliderImg = pygame.image.load('./images/53-Breakout-Tiles.png')
+sliderX = 0
+sliderY = 550
+sliderX_change = 0
+
+# function to build slider image
+def slider(x,y):
+    screen.blit(sliderImg,(x,y))
 
 
 
@@ -27,10 +38,34 @@ while running:
     #screen color 
     screen.fill((0,0,0))
 
+
     # checking event to quit window 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        #adding keys to move slider left and right.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                sliderX_change = -0.6
+            if event.key == pygame.K_RIGHT:
+                sliderX_change = 0.6
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                sliderX_change = 0
+                
     
+    #slider controlling movement at the end of screen
+    sliderX += sliderX_change
+    if sliderX <=0 :
+        sliderX = 0
+    elif sliderX >= 680:
+        sliderX = 680
+
+    #rendering slider.
+    slider(sliderX,sliderY)
+            
+
     #updating display every time
     pygame.display.update()
